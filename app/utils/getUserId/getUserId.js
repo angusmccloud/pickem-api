4a0cad862b76ffcb6da2d8e017473224370669b9
Connor Tyrrell <connor@brewgene.com>
2021-07-19 20:43:36 -0400

    Temporarily set to always send back one userId until I add Auth to the project

--- app/utils/getUserId/getUserId.js
+++ app/utils/getUserId/getUserId.js
@@ -4,9 +4,16 @@ const AWS = require('aws-sdk');
 AWS.config.setPromisesDependency(require('bluebird'));
 const jwt = require('jsonwebtoken');
 
+////
+// To-Do:
+// Change this back
+// And check token validity
+////
+
 const getUserId = (jwtToken) => {
-  const decoded = jwt.decode(jwtToken);
-  return decoded.sub;
+  // const decoded = jwt.decode(jwtToken);
+  // return decoded.sub;
+  return '980a0f9e-ede3-4d7b-a195-6367e68d3d40';
 };
 
 module.exports = getUserId;
