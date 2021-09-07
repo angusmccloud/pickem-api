d1349ffb505b641d5dba992dc33178b69900c486
Connor Tyrrell <connor@brewgene.com>
2021-09-07 06:49:06 -0400

    Fixed a Typo in the gameAdmin check

--- app/utils/getUserId/getUserId.js
+++ app/utils/getUserId/getUserId.js
@@ -12,7 +12,7 @@ const getUserId = (jwtToken) => {
     validUser: true,
     userId,
     admin: userId === '17432f6a-5442-480c-97a1-896172a0821f',
-    gameAdmin: userId === '17432f6a-5442-480c-97a1-896172a0821f' || userID === 'c22a77cd-afa2-4559-8469-79af4c01fe84' || userId === '8d004187-4844-4bea-8685-05da79005d30',
+    gameAdmin: userId === '17432f6a-5442-480c-97a1-896172a0821f' || userId === 'c22a77cd-afa2-4559-8469-79af4c01fe84' || userId === '8d004187-4844-4bea-8685-05da79005d30',
   };
 };
 
