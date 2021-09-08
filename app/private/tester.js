dc6e5ec18c13d4bf758a6f96d1a842b23356592a
Connor Tyrrell <connor@brewgene.com>
2021-09-08 11:24:23 -0400

    Fixed issue with CognitoGetAllUsers only fetching 60 rows

--- app/private/tester.js
+++ app/private/tester.js
@@ -51,7 +51,7 @@ module.exports.tester = async () => {
   //   console.log(5
   // }
   // return await getStandings(1);
-  // return await getPicks(1, 5, '2ce7edd7-87aa-4638-9d9b-c9a4e77d5b84', true);
+  return await getPicks(1, 1, undefined, true);
   // return await getPicksByWeek(1, '2ce7edd7-87aa-4638-9d9b-c9a4e77d5b84');
   // return await getUsers(1);
   // return await getUser(1, '980a0f9e-ede3-4d7b-a195-6367e68d3d40');
@@ -60,7 +60,7 @@ module.exports.tester = async () => {
   // return await dynamoScanAllRows('testTable', 'testTable');
   // return await cognitoGetAllUsers();
 
-  return await getGamesByWeek(1);
+  // return await getGamesByWeek(1);
 
   ////
   // Load up fake picks for a few users
--- app/utils/cognitoGetAllUsers/cognitoGetAllUsers.js
+++ app/utils/cognitoGetAllUsers/cognitoGetAllUsers.js
@@ -3,35 +3,48 @@
 const AWS = require('aws-sdk');
 const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider();
 
-////
-// This might only fetch 60 rows (like Scan does)
-// Check as the site builds out
-////
-
 const cognitoGetAllUsers = async () => {
-  const params = {
-    UserPoolId: process.env.USER_POOL_ID
-  };
-
+  let fetchMoreData = true;
   const users = [];
+  let startKey;
+  let params;
 
-  const result = await cognitoidentityserviceprovider.listUsers(params).promise();
-  if (result.Users !== undefined || result.Users.length !== 0) {
-    // console.log(result);
-    for (let i = 0; i < result.Users.length; i++) {
-      const user = result.Users[i];
-      const attributes = user.Attributes;
-      const userId = attributes.find(attribute => attribute.Name === 'sub').Value;
-      const email = attributes.find(attribute => attribute.Name === 'email').Value;
-      if(user.Enabled) {
-        users.push({
-          userId,
-          email,
-          username: user.Username
-        });
-      }
+  while (fetchMoreData){
+    if (!startKey) {
+      params = {
+        UserPoolId: process.env.USER_POOL_ID
+      };
+    } else {
+      params = {
+        UserPoolId: process.env.USER_POOL_ID,
+        PaginationToken: startKey
+      };
+    }
+
+    const result = await cognitoidentityserviceprovider.listUsers(params).promise();
+    if (result.Users !== undefined || result.Users.length !== 0) {
+      // console.log('---- COGNITO RESULT ----', result);
+      for (let i = 0; i < result.Users.length; i++) {
+        const user = result.Users[i];
+        const attributes = user.Attributes;
+        const userId = attributes.find(attribute => attribute.Name === 'sub').Value;
+        const email = attributes.find(attribute => attribute.Name === 'email').Value;
+        if(user.Enabled) {
+          users.push({
+            userId,
+            email,
+            username: user.Username
+          });
+        }
+      };
     };
-  };
+
+    if(result.PaginationToken){
+      startKey = result.PaginationToken;
+    } else {
+      fetchMoreData = false;
+    }
+  }
 
   return users;
 };
